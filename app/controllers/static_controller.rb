ce845272274203c00d7b6e48ca99d9be6796f4e5
Murarius <murarius2011@gmail.com>
2016-03-09 13:27:21 +0100

    change root path to show all posts

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,6 @@
+# class StaticController < ApplicationController
 class StaticController < ApplicationController
   def home
+    @posts = Post.all
   end
 end
--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -3,10 +3,19 @@ require 'rails_helper'
 RSpec.describe StaticController, type: :controller do
   render_views
 
+  before do
+    @post = FactoryGirl.create(:post)
+  end
+
   describe 'GET #home' do
     it 'returns http success' do
       get :home
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns post title' do
+      get :home
+      expect(response.body).to have_content(@post.title)
     end
   end
 end
