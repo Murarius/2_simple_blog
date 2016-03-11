fc003531fe771443d940dee8f0c200afde4d5a3b
Murarius <murarius2011@gmail.com>
2016-03-11 14:07:31 +0100

    add update and create with tests, add user must be logged in to use actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 # class PostsController < ApplicationController
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   def show
     @post = Post.find(params[:id])
   end
@@ -8,7 +9,31 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to root_path, notice: 'Post created.'
+    else
+      render 'new'
+    end
+  end
+
   def edit
     @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to root_path, notice: 'Post updated.'
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit([:title, :content, :more_content])
   end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -8,34 +8,133 @@ RSpec.describe PostsController, type: :controller do
   end
 
   describe 'GET #show' do
-    it 'with success' do
+    it 'returns success' do
       get :show, id: @post.id
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
       expect(response.body).to have_content(@post.title)
     end
 
-    it 'rerurns more content field' do
+    it 'returns more_content field' do
       get :show, id: @post.id
       expect(response.body).to have_content(@post.more_content)
     end
   end
 
   describe 'GET #new' do
-    it 'with success' do
+    it 'when logged in returns success' do
+      log_in(@post.user, no_capybara: true)
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'when not logged in redirects' do
+      get :new
+      expect(response).to have_http_status(302)
     end
   end
 
-  describe 'POST #create'
+  describe 'POST #create' do
+    describe 'after log in' do
+      before do
+        log_in(@post.user, no_capybara: true)
+        @request.env['devise.mapping'] = Devise.mappings[:user]
+      end
+
+      describe 'with valid data' do
+        it 'adds new record' do
+          expect do
+            post :create, post: { title: 'Lorem', content: 'Lorem', more_content: 'Lorem' }
+          end.to change { Post.all.count }.by(1)
+        end
+
+        it 'redirects to root_path' do
+          post :create, post: { title: 'Lorem', content: 'Lorem', more_content: 'Lorem' }
+          expect(response).to redirect_to(root_path)
+          expect(response).to have_http_status(302)
+        end
+      end
+
+      describe 'with invalid data' do
+        before do
+          @post_hash = { title: 'Lorem' }
+          @post_obj = Post.new(@post_hash)
+        end
+
+        it 'rerenders new template with error messages' do
+          @post_obj.validate
+          post :create, post: @post_hash
+          expect(response).to have_http_status(200)
+          expect(response).to render_template('new')
+          # first error in response body
+          expect(response.body).to have_content(@post_obj.errors.messages.to_a[0].join(' '))
+        end
+
+        it 'dont add new record' do
+          expect do
+            post :create, post: @post_hash
+          end.to change { Post.all.count }.by(0)
+        end
+      end
+    end
+
+    describe 'when not logged in' do
+      it 'redirects' do
+        post :create, post: { title: 'Lorem', content: 'Lorem', more_content: 'Lorem' }
+        expect(response).to have_http_status(302)
+      end
+    end
+  end
 
   describe 'GET #edit' do
-    it 'with success' do
+    it 'when logged in returns success' do
+      log_in(@post.user, no_capybara: true)
       get :edit, id: @post.id
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'when not logged in redirects' do
+      get :edit, id: @post.id
+      expect(response).to have_http_status(302)
     end
   end
 
-  describe 'PATCH #update'
+  describe 'PATCH #update' do
+    describe 'after log in' do
+      before do
+        log_in(@post.user, no_capybara: true)
+        @request.env['devise.mapping'] = Devise.mappings[:user]
+      end
+
+      describe 'with valid data' do
+        it 'redirects to root' do
+          patch :update, id: @post.id, post: { title: 'New Lorem' }
+          expect(response).to have_http_status(302)
+          expect(response).to redirect_to(root_path)
+        end
+
+        it 'updates record' do
+          patch :update, id: @post.id, post: { title: 'New Lorem' }
+          expect(@post.reload.title).to eq 'New Lorem'
+        end
+      end
+
+      describe 'with invalid data' do
+        it 'rerenders edit template with error messages' do
+          patch :update, id: @post.id, post: { title: '' }
+          expect(response).to have_http_status(200)
+          expect(response).to render_template('edit')
+          expect(response.body).to have_content("can't be blank")
+        end
+      end
+    end
+
+    describe 'when not logged in' do
+      it 'redirects' do
+        patch :update, id: @post.id, post: { title: 'New Lorem' }
+        expect(response).to have_http_status(302)
+      end
+    end
+  end
+
   describe 'DELETE #destroy'
 end
