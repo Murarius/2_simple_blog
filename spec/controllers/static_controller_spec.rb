4abae237a61bba314ac98f28f40770dd29aac170
Murarius <murarius2011@gmail.com>
2016-03-14 14:25:52 +0100

    add post partial and post controll buttons partial with tests

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -17,5 +17,26 @@ RSpec.describe StaticController, type: :controller do
       get :home
       expect(response.body).to have_content(@post.title)
     end
+
+    it 'has no post edit/delete buttons when not logged in' do
+      get :home
+      expect(response.body).to have_link('', href: '/login')
+      expect(response.body).not_to have_link('', href: '/logout')
+      # expect(response.body).not_to have_link('', href: edit_post_path(@post))
+      # expect(response.body).not_to have_link('', href: post_path(@post))
+      expect(response.body).not_to have_xpath("//a[@href='#{edit_post_path(@post)}' and @class='button edit']")
+      expect(response.body).not_to have_xpath("//a[@href='#{post_path(@post)}' and @class='button delete']")
+
+    end
+
+    it 'has post edit/delete buttons when logged in' do
+      log_in(@post.user, no_capybara: true)
+      get :home
+      expect(response.body).not_to have_link('', href: '/login')
+      expect(response.body).to have_link('', href: '/logout')
+      expect(response.body).to have_xpath("//a[@href='#{edit_post_path(@post)}' and @class='button edit']")
+      expect(response.body).to have_xpath("//a[@href='#{post_path(@post)}' and @class='button delete']")
+    end
+
   end
 end
