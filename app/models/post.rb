7214a8824cf60932bd105a33b2a49c9c029e6bc2
Murarius <murarius2011@gmail.com>
2016-03-17 15:15:12 +0100

    move count scopes to private, strip month names

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,23 +7,9 @@ class Post < ActiveRecord::Base
   validates :user, presence: true
 
   scope :order_by_created_at, -> { order('created_at DESC') }
-  scope :counts_by_year, -> do
-    select("to_char(created_at, 'YYYY') as year, count(*) as count")
-      .group('year')
-      .order('year DESC')
-  end
-
+  scope :new_posts, -> { order('created_at').limit(5) }
   scope :from_year, ->(year = nil) do
     where("to_char(created_at, 'YYYY') = ?", year.to_s) if year
-  end
-
-  scope :new_posts, -> { order('created_at').limit(5) }
-
-  scope :counts_by_month, ->(year) do
-    select("to_char(created_at, 'Month') as month, to_char(created_at, 'MM') as month_number, count(*) as count")
-      .where("to_char(created_at, 'YYYY') = ?", year.to_s)
-      .group('month, month_number')
-      .order('month_number')
   end
 
   def self.post_counts_by_year
@@ -31,6 +17,21 @@ class Post < ActiveRecord::Base
   end
 
   def self.post_counts_by_month(year)
-    counts_by_month(year).map { |record| { month: record.month, count: record.count } }
+    counts_by_month(year).map { |record| { month: record.month.strip, count: record.count } }
+  end
+
+  private
+
+  scope :counts_by_year, -> do
+    select("to_char(created_at, 'YYYY') as year, count(*) as count")
+      .group('year')
+      .order('year DESC')
+  end
+
+  scope :counts_by_month, ->(year) do
+    select("to_char(created_at, 'Month') as month, to_char(created_at, 'MM') as month_number, count(*) as count")
+      .where("to_char(created_at, 'YYYY') = ?", year.to_s)
+      .group('month, month_number')
+      .order('month_number')
   end
 end
