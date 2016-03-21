be40e1b1c6777423ac5041b5a16a56d5938528b7
Murarius <murarius2011@gmail.com>
2016-03-21 13:30:31 +0100

    change view specs form erb to haml

---
+++ spec/views/sidebar/_new_posts.html.haml_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'sidebar/_new_posts.html.haml', type: :view do
+  before do
+    @user = FactoryGirl.create(:user)
+    @new_posts = []
+    5.times { @new_posts.push(FactoryGirl.create(:post, user_id: @user.id)) }
+  end
+
+  it 'displays 5 newest posts' do
+    assign(:new_posts, @new_posts)
+    render
+    expect(rendered).to have_content(@new_posts.first.title)
+    expect(rendered).to have_content(time_ago_in_words(@new_posts.first.created_at))
+  end
+end
