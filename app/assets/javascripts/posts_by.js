b3ca082ebd33820659100ed0a1777260ae1a9e59
Murarius <murarius2011@gmail.com>
2016-03-21 12:32:37 +0100

    add sidebar post_by_year jquery loading

--- app/assets/javascripts/posts_by.js
+++ app/assets/javascripts/posts_by.js
@@ -1,2 +1,54 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function renderMonthsCounts(data, appendToElement){
+  data = $(data).hide();
+  $(appendToElement).append(data);
+  $(data).slideDown();
+}
+
+function animateButton(button){
+  $(button).toggleClass('rotate');
+}
+
+function handleLoadMonthsCountsError(request, errorType, errorMessage){
+  var flash = $('.flash');
+  var newMessage = $("<div class='alert alert-alert'>" + errorType + ': ' + errorMessage + '</div>');
+  var existingMessage = $(flash.find('.alert')[0]);
+  if (newMessage.text() != existingMessage.text()) {
+    flash.append(newMessage);
+  }
+}
+
+function loadMonthsCounts(button){
+  var toMonthsCountsLink = button;
+  var year=$(toMonthsCountsLink).attr('id');
+  $.ajax('posts_by/'+year, {
+    success: function(response) {
+      renderMonthsCounts(response, $(toMonthsCountsLink).parent());
+      animateButton(toMonthsCountsLink);
+    },
+    error: handleLoadMonthsCountsError,
+    timeout: 4000,
+    beforeSend: function() {
+      $(toMonthsCountsLink).after($("<i class='fa fa-spinner fa-spin'></i>"));
+    },
+    complete: function() {
+      $(toMonthsCountsLink).next().remove();
+    }
+  });
+}
+
+$(function() {
+  $('.sidebar').on('click', '.months', function(e){
+    e.preventDefault();
+    console.log($(this).attr('class'));
+    if ($(this).attr('class') == 'months rotate') {
+      animateButton(this);
+      var monthsCountsMenu = $(this).parent().find('ul');
+      $(monthsCountsMenu).slideUp();
+    } else {
+      loadMonthsCounts(this);
+    }
+  });
+});
