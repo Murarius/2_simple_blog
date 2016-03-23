9d21b2eeb36d44caf072a183dc7f6f32d9bcee03
Murarius <murarius2011@gmail.com>
2016-03-23 10:06:16 +0100

    add devise local controllers with routing tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   root 'static#home'
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+  devise_for :users,
+             path: '',
+             path_names: { sign_in: 'login', sign_out: 'logout' },
+             controllers: { passwords: 'users/passwords', sessions: 'users/sessions' }
   resources :posts, except: :index
   get 'posts_by/:year', to: 'posts_by#year_posts_months_counts'
 end
--- spec/routing/session_spec.rb
+++ spec/routing/session_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Session', type: :routing do
   it 'responds to login' do
-    expect(get: '/login').to route_to('devise/sessions#new')
+    expect(get: '/login').to route_to('users/sessions#new')
   end
 
   it 'responds to logout' do
-    expect(delete: '/logout').to route_to('devise/sessions#destroy')
+    expect(delete: '/logout').to route_to('users/sessions#destroy')
   end
 end
