ffd917fd15152d3ea069cd211596fc9df9d1a4ed
Murarius <murarius2011@gmail.com>
2016-03-24 09:11:38 +0100

    finish sidebar cache feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+# class ApplicationController < ActionController::Base
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -7,14 +8,19 @@ class ApplicationController < ActionController::Base
     cookies[:sidebar] = render_to_string(template: 'sidebar/sidebar.json.jbuilder', locals: { sidebar: sidebar })
   end
 
-  def load_sidebar
-    if cookies[:sidebar]
-      sidebar_cookie = JSON.parse(cookies[:sidebar], symbolize_names: true)[:sidebar]
-      sidebar = Sidebar.new(sidebar_cookie[:new_posts], sidebar_cookie[:posts_counts_by_years])
-    else
-      sidebar = Sidebar.new
-      sidebar_to_cookie(sidebar)
-    end
+  def sidebar_mark_to_cookie
+    cookies[:sidebar_mark] = AppConfig.sidebar_mark
+  end
+
+  def create_new_sidebar
+    sidebar = Sidebar.new
+    sidebar_to_cookie(sidebar)
+    sidebar_mark_to_cookie
     sidebar
+  end
+
+  def load_sidebar
+    return create_new_sidebar unless cookies[:sidebar] && AppConfig.valid_sidebar_mark?(cookies[:sidebar_mark])
+    Sidebar.new_from_cookie(cookies[:sidebar])
   end
 end
--- app/models/app_config.rb
+++ app/models/app_config.rb
@@ -1,7 +1,16 @@
 # class AppConfig < ActiveRecord::Base
 class AppConfig < ActiveRecord::Base
   def self.refresh_sidebar_mark
-    first.update(sidebar_mark: generate_sidebar_mark)
+    first_or_create(sidebar_mark: generate_sidebar_mark)
+  end
+
+  def self.sidebar_mark
+    first.try(:sidebar_mark)
+  end
+
+  def self.valid_sidebar_mark?(mark)
+    return false if first.try(sidebar_mark) != mark
+    true
   end
 
   def self.generate_sidebar_mark
