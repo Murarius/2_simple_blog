8ea7f6f0c795a7420f3c94a43cf5d8850693fb4c
Murarius <murarius2011@gmail.com>
2016-04-07 14:37:02 +0200

    add comment validation and json format with tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,15 @@
 class Comment < ActiveRecord::Base
+  require 'action_view'
+  require 'action_view/helpers'
+  include ActionView::Helpers::DateHelper
+
   belongs_to :post
+
+  validates :author, presence: true
+  validates :post, presence: true
+  validates :content, presence: true
+
+  def as_json(options = {})
+    { id: id, author: author, content: content, created_at: time_ago_in_words(created_at) + ' ago' }
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,4 +7,32 @@ RSpec.describe Comment, type: :model do
     expect(subject).to respond_to(:post)
     expect(subject).to respond_to(:created_at)
   end
+
+  describe 'to be valid' do
+    let(:comment) { FactoryGirl.build(:comment) }
+
+    it 'must have author' do
+      expect { comment.author = nil }.to change { comment.valid? }.from(true).to(false)
+    end
+
+    it 'must have content' do
+      expect { comment.content = nil }.to change { comment.valid? }.from(true).to(false)
+    end
+
+    it 'must have post' do
+      expect { comment.post = nil }.to change { comment.valid? }.from(true).to(false)
+    end
+  end
+
+  it 'belongs to Post' do
+    post = FactoryGirl.build(:post)
+    comment = FactoryGirl.build(:comment, post: post)
+    expect(comment.post).to eq post
+  end
+
+  it 'returns time ago in words date when json' do
+    comment = FactoryGirl.create(:comment)
+    # somthing like 2 minutes ago <- check if ago exists
+    expect(comment.to_json.include?('ago')).to be true
+  end
 end
