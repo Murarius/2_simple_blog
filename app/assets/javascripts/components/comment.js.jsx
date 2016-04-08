f36e7e17f6de6ba298e39cd8f8ba7a8273b5bfa4
Murarius <murarius2011@gmail.com>
2016-04-08 08:50:32 +0200

    add add_comment feature

--- app/assets/javascripts/components/comment.js.jsx
+++ app/assets/javascripts/components/comment.js.jsx
@@ -9,10 +9,9 @@ var Comment = React.createClass({
       <div className='comment'>
         <div className='comment-header'>
           <div className='comment-author'>{this.state.comment.author}</div>
-          <div className='comment-created_at'>{this.state.comment.created_at}</div>
+          <div className='comment-created-at'>{this.state.comment.created_at}</div>
         </div>
         <div className='comment-content'>{this.state.comment.content}</div>
-        <div className='comment-comments-bar'>add comment</div>
       </div>
     );
   }
--- app/assets/javascripts/components/comments.js.jsx
+++ app/assets/javascripts/components/comments.js.jsx
@@ -1,13 +1,22 @@
 var Comments = React.createClass({
   getInitialState: function() {
     return {
-      comments: this.props.comments
+      postId: this.props.postId,
+      comments: this.props.comments,
+      commentsCount: this.props.comments.length
     };
   },
+  handleNewComment: function(comment) {
+    var comments = this.state.comments;
+    comments.push(comment);
+    this.replaceState({comments: comments, commentsCount: comments.length});
+  },
   render: function() {
+    var self = this;
     return (
       <div className='comments'>
-        <h2>Comments:</h2>
+        <h2>Comments: <CommentsCount>{this.state.commentsCount}</CommentsCount></h2>
+        <AddComment postId={this.state.postId} handleNewComment={self.handleNewComment}></AddComment>
         {this.state.comments.map(function(comment) {
           return (<Comment comment={comment} key={comment.id}></Comment>);
         })}
